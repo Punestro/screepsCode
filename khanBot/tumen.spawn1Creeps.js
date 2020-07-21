4a6ee52121479c09b89e40c6c29266d14a37be61
Punestro <45181238+Punestro@users.noreply.github.com>
2020-07-21 12:05:15 -0500

    Update tumen.spawn1Creeps.js

--- khanBot/tumen.spawn1Creeps.js
+++ khanBot/tumen.spawn1Creeps.js
@@ -1,5 +1,5 @@
-var manageSpawn1Harvesters = require('mingghan.spawn1HarvesterManager') var manageSpawn1Buildier = require('mingghan.spawn1BuilderManager')
-var manageSpawn1Upgraders = require('mingghan.mingghan.spawn1UpgraderManager') 
+var manageSpawn1Harvesters = require('mingghan.spawn1HarvesterManager') var manageSpawn1Builders = require('mingghan.spawn1BuilderManager')
+var manageSpawn1Upgraders = require('mingghan.spawn1UpgraderManager') 
 
 var spawn1Creeps = {
   run: function(manageCreeps)
@@ -16,13 +16,14 @@ var spawn1Creeps = {
   if(Memory.spawn1RoomControllerLevel == 3){
     Memory.upgraderStats = [WORK,CARRY,WORK,CARRY,MOVE,MOVE];
     Memory.builderStats = [WORK,CARRY,WORK,CARRY,MOVE,MOVE];
-    Memory.minerStats = [WORK,WORK,WORK,CARRY,CARRY,MOVE];
+    Memory.minerStats = [WORK,WORK,WORK,WORK,WORK,MOVE,MOVE];
     Memory.fillerStats = [CARRY,CARRY,MOVE,CARRY,CARRY,MOVE,];
     Memory.turretStats = [CARRY,CARRY,MOVE,CARRY,CARRY,MOVE,];
     
   } 
   manageSpawn1Harvesters.run(manageSpawn1Harvesters);
-  manageSpawn1Building.run(manageSpawn1Builders);
+  manageSpawn1Upgraders.run(manageSpawn1Upgraders);
+  manageSpawn1Builders.run(manageSpawn1Builders);
 }
 }
 module.exports = spawn1Creeps
