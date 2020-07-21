1cd914324cfdb8f037e5b69f0e6d841342daba5b
Punestro <45181238+Punestro@users.noreply.github.com>
2020-07-21 09:09:17 -0500

    Update mingghan.spawn1UpgraderManager.js

--- khanBot/mingghan.spawn1UpgraderManager.js
+++ khanBot/mingghan.spawn1UpgraderManager.js
@@ -1,1 +1,7 @@
+var spawn1UpgraderManager = {
+  run: function(manageSpawn1Upgraders){
+  
+    Memory.spawn1BasicUpgraders = _.filter(Game.creeps, (creep) => creep.memory.job == 'spawn1BasicUpgrader'); 
+    Memory.spawn1ContainerUpgrader = _.filter(Game.creeps, (creep) => creep.memory.job == 'spawn1ContainerUpgrader'); 
 
+module.exports = spawn1UpgraderManager;
